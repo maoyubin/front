96a3f342d74d27b439a1c6e6737f234f391778a8
毛宇斌 <maoyubinabc@163.com>
2020-08-16 07:17:12 +0800

    init

---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <el-form>
+            <el-form-item  props="email">
+                <span>
+                    <i class='el-icon-mobile'></i>
+                </span>
+                <el-input placeHolder='用户名'></el-input>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'login'
+}
+</script>
+
+<style lang="stylus">
+
+</style>
