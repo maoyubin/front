c8ff20ed415afe0bdbf959c47d815cb11a13b75d
毛宇斌 <maoyubinabc@163.com>
2020-08-20 21:37:40 +0800

    upload file

--- pages/uc.vue
+++ pages/uc.vue
@@ -1,15 +1,87 @@
 <template>
     <div>
         <h1>用户中心</h1>
+
+        <div ref='drag' id="drag">
+            <input type="file" name="file" @change="handleFileChnage">
+        </div>
+
+        <div>
+            <el-progress :stroke-width='20' :text-inside='true'  :percentage="uploadProgress" ></el-progress>
+        </div>
+        <div>
+            <el-button @click="uploadFile">upLoad</el-button>
+        </div>
+
     </div>
 </template>
 
+<style lang="stylus">
+#drag
+    height 100px
+    line-height 100px
+    border 2px dashed #eee
+    text-align center
+    vertical-align middle
+    
+</style>
+
 <script>
+//&:hover
+//        border-color red
 export default {
     mounted(){
         const ret = this.$http.get('/user/info')
 
-        console.log('11111111',ret)
+        this.bindEvents()
+    },
+    data(){
+        return {
+            file: null,
+            uploadProgress: 0
+        }
+    },
+    methods:{
+        bindEvents(){
+            const drag = this.$refs.drag
+            drag.addEventListener('dragover', e=>{
+                drag.style.borderColor = 'red'
+                e.preventDefault()
+            })
+
+            drag.addEventListener('dragleave', e=>{
+                drag.style.borderColor = '#eee'
+                e.preventDefault()
+            })
+
+            drag.addEventListener('drop', e=>{
+                e.preventDefault()
+                const fileList = e.dataTransfer.files
+                drag.style.borderColor = '#eee'
+                this.file = fileList[0]
+            })
+        },
+        async uploadFile(){
+            const form = new FormData()
+            form.append('name','file')
+            form.append('file',this.file)
+
+            const ret = await this.$http.post('/uploadfile', form, {
+                onUploadProgress:progress=>{
+                    this.uploadProgress = Number(((progress.loaded/progress.total)*100).toFixed(2))
+                }
+            })
+            console.log(ret)
+        },
+
+        handleFileChnage(e){
+            const [file] = e.target.files
+            if(!file) return 
+
+            this.file = file
+
+
+        }
     }
 }
 </script>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import axios from 'axios'
-
+import {MessageBox} from 'element-ui'
 const service = axios.create({
     baseURL:'/api'
 })
 
 Vue.prototype.$http = service
 
-//请求拦截
+
+export default ({store, redirect}) =>{
+    //请求拦截
 //主要做token
 service.interceptors.request.use(
     async config=>{
@@ -24,9 +26,23 @@ service.interceptors.response.use(
     async response=>{
         let {data} = response
 
+        if(data.code === -666){
+            MessageBox.confirm('登录也过期','过期',{
+                confirmButtonText:'登录',
+                showCancelButton:false
+            }).then(()=>{
+                localStorage.removeItem('token')
+                redirect({path:'login'})
+            })
+        }
+
         return data
     }
 )
+
+
+}
+
 
 
 export const http = service
